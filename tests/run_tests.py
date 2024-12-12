3ddbf144fdcfb017a1460657df1513faceb21bbf
Calbabreaker <calbabreaker@gmail.com>
2024-12-12 14:59:32 +1030

    ci: macos not workin for some resaion

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -61,7 +61,7 @@ for test_dir in args.tests:
     if not os.path.exists("./Makefile"):
         exec_shell("cmake ..")
 
-    exec_shell(f"make -j{multiprocessing.cpu_count()}")
+    exec_shell(f"cmake --build . --parallel {multiprocessing.cpu_count()}")
 
     if not args.run:
         continue
